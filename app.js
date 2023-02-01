552425eb85408eb5563a00b84366959e665d63ec
LeanBono <bonolean@gmail.com>
2023-02-01 19:14:21 -0300

    se agrega el proyecto

---
+++ app.js
@@ -0,0 +1,7 @@
+document.getElementById('btn').addEventListener('click',function(){
+    document.body.style.backgroundColor = '#3b9da0'
+})
+
+document.getElementById('btn_eliminar').addEventListener('click',function(){
+    document.body.style.backgroundColor = '#000000'
+})
